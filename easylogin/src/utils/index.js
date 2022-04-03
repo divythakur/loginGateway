30761329fb38f8cde8006bc4019dfbc643f29792
Sriashika Addala <ashika29@users.noreply.github.com>
2022-04-03 22:26:12 +0530

    Sign-in option functions added as utils

---
+++ easylogin/src/utils/index.js
@@ -0,0 +1,4 @@
+export { defaultJWTSignIn } from "./sign-ins/default";
+export { googleSignIn } from "./sign-ins/google";
+export { microsoftSignIn } from "./sign-ins/microsoft";
+export { singleSignOn } from "./sign-ins/sso";
