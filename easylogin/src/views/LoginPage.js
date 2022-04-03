9309ef1d030066456a26102b6c101f730c1fe00c
divythakur <divyanshuthakur75@gmail.com>
2022-04-03 20:43:50 +0530

    installed packages'

---
+++ easylogin/src/views/LoginPage.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {Typography} from '@material-ui/core'
+
+class LoginPage extends Component {
+    // constructor(props) {
+    //     super(props);
+
+    // }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+          <h1>Welcome to the login page</h1>
+            </div>
+        );
+    }
+}
+
+LoginPage.propTypes = {
+
+};
+
+export default LoginPage;
